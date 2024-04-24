23f6f0e839482112e490240b7f542a1b41de6bcf
Rodrigo <rodrigogranemannmelo2006@gmail.com>
2024-04-24 20:08:19 -0300

    Added email command.

--- js/commands.js
+++ js/commands.js
@@ -4,7 +4,7 @@ input.addEventListener('keypress', function (event) {
     if (input.value === "Type 'help' to print all the commands") {
         input.value = '';
     } else if (event.key === "Enter" && input.value === "help") {
-        window.alert("help\t print all the commands.\nblog\t redirects to my blog.\nresume\t shows my resume.\nlinkedin\t redirects to my linkedin profile.\ngithub\tredirects to my github profile.\ngoodreads\tredirects to my goodreads profile.");
+        window.alert("help\t print all the commands.\nblog\t redirects to my blog.\nresume\t shows my resume.\nlinkedin\t redirects to my linkedin profile.\ngithub\tredirects to my github profile.\ngoodreads\tredirects to my goodreads profile.\nemail\tto contact me.");
     } else if (event.key === "Enter" && input.value === "blog") {
         location.href = "archives/index.html";
     } else if (event.key === "Enter" && input.value == "resume") {
@@ -15,5 +15,7 @@ input.addEventListener('keypress', function (event) {
         location.href = "https://github.com/pontarolo";
     } else if (event.key === "Enter" && input.value == "goodreads") {
         location.href = "https://www.goodreads.com/user/show/177224558-rodrigo-melo";
+    } else if (event.key === "Enter" && input.value == "email") {
+        location.href = "mailto:rodrigo@pontarolo.org";
     }
 });
