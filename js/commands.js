8ed47a775db5c412966972d22443374969637e3d
Rodrigo <rodrigogranemannmelo2006@gmail.com>
2024-04-24 18:38:09 -0300

    New website visual.

---
+++ js/commands.js
@@ -0,0 +1,19 @@
+const input = document.getElementById('cli');
+
+input.addEventListener('keypress', function (event) {
+    if (input.value === "Type 'help' to print all the commands") {
+        input.value = '';
+    } else if (event.key === "Enter" && input.value === "help") {
+        window.alert("help\t print all the commands.\nblog\t redirects to my blog.\nresume\t shows my resume.\nlinkedin\t redirects to my linkedin profile.\ngithub\tredirects to my github profile.\ngoodreads\tredirects to my goodreads profile.");
+    } else if (event.key === "Enter" && input.value === "blog") {
+        location.href = "archives/index.html";
+    } else if (event.key === "Enter" && input.value == "resume") {
+        location.href = "";
+    } else if (event.key === "Enter" && input.value == "linkedin") {
+        location.href = "https://www.linkedin.com/in/rodrigo-granemann/";
+    } else if (event.key === "Enter" && input.value == "github") {
+        location.href = "https://github.com/pontarolo";
+    } else if (event.key === "Enter" && input.value == "goodreads") {
+        location.href = "https://www.goodreads.com/user/show/177224558-rodrigo-melo";
+    }
+});
